5841ffdea2b8445abc8015085375ccd93e5bf114
Kevin Akahoshi <kevin.akahoshi@gmail.com>
2020-01-29 02:15:39 +0000

    Used boilerplate from LFZ

--- server/index.js
+++ server/index.js
@@ -1,13 +1,23 @@
 require('dotenv/config');
 const express = require('express');
+
+const db = require('./database');
+const ClientError = require('./client-error');
 const staticMiddleware = require('./static-middleware');
 const sessionMiddleware = require('./session-middleware');
-const ClientError = require('./client-error');
+
 const app = express();
 
 app.use(staticMiddleware);
 app.use(sessionMiddleware);
+
 app.use(express.json());
+
+app.get('/api/health-check', (req, res, next) => {
+  db.query('select \'successfully connected\' as "message"')
+    .then(result => res.json(result.rows[0]))
+    .catch(err => next(err));
+});
 
 app.use('/api', (req, res, next) => {
   next(new ClientError(`cannot ${req.method} ${req.originalUrl}`, 404));
