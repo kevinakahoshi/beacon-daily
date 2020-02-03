6ec916b813bbb7c6328356ef1a5eb40f0f6acaec
Kevin Akahoshi <kevin.akahoshi@gmail.com>
2020-02-03 17:27:23 +0000

    Added material UI

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -2,10 +2,21 @@ import React from 'react';
 import {
 // Link
 } from 'react-router-dom';
+import {
+  Box,
+  Drawer
+} from '@material-ui/core';
 
 function Header() {
   return (
-    <div>This is the header.</div>
+    <>
+      <Drawer>
+
+      </Drawer>
+      <Box bgcolor="primary.main" color="primary.contrastText">
+        <h1>Beacon Daily</h1>
+      </Box>
+    </>
   );
 }
 
